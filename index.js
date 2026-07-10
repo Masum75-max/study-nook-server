8eccfa538796082eda351c1c0963d66bb90fab0d
Masum75-max <almasumabdullah414@gmail.com>
2026-07-10 20:08:22 +0600

    Commenting connection

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
-     await client.connect();
+  //   await client.connect();
     
     const db=await client.db("book-nook")
     const roomsCollection = await db.collection("rooms")
